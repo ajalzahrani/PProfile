617e0e3e126374b68fac254c053a9bbbb55afcc9
Abdulrhaman Alzahrani <alzahrani.abdulrahman3@gmail.com>
2026-01-26 08:14:44 +0300

    Merge branch 'main' of https://github.com/ajalzahrani/PProfile

--- src/app/documents-config/components/requirement-toggle.tsx
+++ src/app/documents-config/components/requirement-toggle.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { updateCertificateRequirement } from "@/actions/document-configs";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { DocumentConfigFormValues } from "@/actions/document-configs.validation";
 import { toast } from "@/components/ui/use-toast";
-import { useRouter } from "next/navigation";
 
 export function RequirementToggle({ jobId, catId, initialData }: any) {
   const [isRequired, setIsRequired] = useState(
